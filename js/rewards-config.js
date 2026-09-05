db4162f77dd2a6f6b2f6e5853863f7713719af5a
moneshtech <moneshsys@gmail.com>
2026-09-05 18:31:52 +0530

    Update rewards-config.js

--- js/rewards-config.js
+++ js/rewards-config.js
@@ -1,4 +1,4 @@
 window.MONESH_REWARD_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyM-BtpyoxPTmXaKWFa85BN4x2gDcK5abzxWeR0MRGLXkwr07yvSY1FWwJ6_cPWLS0/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzlf_4TUGO7V-ag40RHJ7PXD1MjYN9a790GuU5h1VEz6WpEUOYxQv6Yi7ta3iLU3Wxz/exec",
   YOUTUBE_CHANNEL_URL: "https://www.youtube.com/@techblueprint-01"
 };
