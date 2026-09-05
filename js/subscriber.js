3d046bb483bf4ab609d7b979b6465d5fabbf2726
moneshtech <moneshsys@gmail.com>
2026-09-05 17:52:31 +0530

    Update subscriber.js

--- js/subscriber.js
+++ js/subscriber.js
@@ -1,5 +1,5 @@
 const COUPON_API =
-  "https://script.google.com/macros/s/AKfycbzP9DmdaONAVA23WF21aLvm3OdEKu0BnT3wLbE-okTA-2Jlf4-JckOYWUyJ9XP4wvI/exec";
+  "https://script.google.com/macros/s/AKfycbyYa-Xmt8AOwHzmZcQ0Lz0xYWUDZNZuhcqlPas7yFLwx_ullPI2ecr0duZrGzgrA7nt/exec";
 
 const YOUTUBE_URL =
   "https://www.youtube.com/@techblueprint-01";
