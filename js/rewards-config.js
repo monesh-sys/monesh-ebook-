80b425373be83f4094cdadab70db0e5c92170a57
moneshtech <moneshsys@gmail.com>
2026-09-05 17:56:47 +0530

    Update rewards-config.js

--- js/rewards-config.js
+++ js/rewards-config.js
@@ -1,7 +1,4 @@
-// Monesh Ebooks public reward configuration.
-// Replace these two placeholders after setting up Google OAuth + Apps Script.
 window.MONESH_REWARD_CONFIG = {
-  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com",
-  APPS_SCRIPT_URL: "YOUR_APPS_SCRIPT_WEB_APP_URL",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwEbNIEyBW3vwDEXzomg5cgSJGVaAM_B72yvjpqy1X3KWfh6iWoLWmvHizLQfPzvJPa/exec",
   YOUTUBE_CHANNEL_URL: "https://www.youtube.com/@techblueprint-01"
 };
